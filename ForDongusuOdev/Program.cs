2d84774069f206eb6c0fdeebf2c47a628f94e726
Hüseyin SEVİM <hsyn_svm@hotmail.com>
2023-11-17 21:51:38 +0300

    Good Luck ;)
    A fun guessing game ;)

---
+++ ForDongusuOdev/Program.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ForDongusuOdev
+{
+    internal class Program
+    {
+        private static void SuperMario()
+        {
+            Console.Beep(659, 125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(523, 125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(784, 125);
+            Thread.Sleep(375);
+            Console.Beep(392, 125);
+            Thread.Sleep(375);
+            Console.Beep(523, 125);
+            Thread.Sleep(250);
+            Console.Beep(392, 125);
+            Thread.Sleep(250);
+            Console.Beep(330, 125);
+            Thread.Sleep(250);
+            Console.Beep(440, 125);
+            Thread.Sleep(125);
+            Console.Beep(494, 125);
+            Thread.Sleep(125);
+            Console.Beep(466, 125);
+            Thread.Sleep(42);
+            Console.Beep(440, 125);
+            Thread.Sleep(125);
+            Console.Beep(392, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(784, 125);
+            Thread.Sleep(125);
+            Console.Beep(880, 125);
+            Thread.Sleep(125);
+            Console.Beep(698, 125);
+            Console.Beep(784, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(587, 125);
+            Console.Beep(494, 125);
+            Thread.Sleep(125);
+            Console.Beep(523, 125);
+            Thread.Sleep(250);
+            Console.Beep(392, 125);
+            Thread.Sleep(250);
+            Console.Beep(330, 125);
+            Thread.Sleep(250);
+            Console.Beep(440, 125);
+            Thread.Sleep(125);
+            Console.Beep(494, 125);
+            Thread.Sleep(125);
+            Console.Beep(466, 125);
+            Thread.Sleep(42);
+            Console.Beep(440, 125);
+            Thread.Sleep(125);
+            Console.Beep(392, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(784, 125);
+            Thread.Sleep(125);
+            Console.Beep(880, 125);
+            Thread.Sleep(125);
+            Console.Beep(698, 125);
+            Console.Beep(784, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(587, 125);
+            Console.Beep(494, 125);
+            Thread.Sleep(375);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(415, 125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(698, 125);
+            Thread.Sleep(125);
+            Console.Beep(698, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(625);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(415, 125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(622, 125);
+            Thread.Sleep(250);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(523, 125);
+            Thread.Sleep(1125);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(415, 125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(698, 125);
+            Thread.Sleep(125);
+            Console.Beep(698, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(625);
+            Console.Beep(784, 125);
+            Console.Beep(740, 125);
+            Console.Beep(698, 125);
+            Thread.Sleep(42);
+            Console.Beep(622, 125);
+            Thread.Sleep(125);
+            Console.Beep(659, 125);
+            Thread.Sleep(167);
+            Console.Beep(415, 125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 125);
+            Console.Beep(523, 125);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(622, 125);
+            Thread.Sleep(250);
+            Console.Beep(587, 125);
+            Thread.Sleep(250);
+            Console.Beep(523, 125);
+            Thread.Sleep(625);
+        }
+        
+        static void Main(string[] args)
+        {
+            #region 0-100 arası çift sayılar
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine($"0-100 Arası Çift sayılar: {i}");
+            //    }
+            //}
+            #endregion
+
+            #region Kullanıcıdan alınan veri ile 0-20 arası çift sayılar
+            //Console.WriteLine("Lütfen bir sayı giriniz: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine()); 
+
+            //for (sayi = 0; sayi < 20; sayi++)
+            //{
+            //    if (sayi %2 == 0)
+            //    {
+            //        Console.WriteLine("Çift sayılar: " + sayi);
+            //    }
+            //}           
+
+            #endregion
+
+            #region Kullanıcıdan alınan veriler ile 4'e bölünebilen sayılar
+            //Console.WriteLine("Lütfen bir sayı giriniz: ");
+            //int sayı = Convert.ToInt32(Console.ReadLine());
+            //int toplam = 0;
+            //int adet = 0;
+
+            //for (sayı = 0; sayı < 20; sayı++)
+            //{
+            //    if (sayı %4 == 0)
+            //    {
+            //        Console.WriteLine("Çift sayılar: " + sayı);
+            //        toplam += sayı;
+            //        Console.WriteLine("Toplamı: " + toplam);
+            //        adet++;
+            //        Console.WriteLine("Adet: " + adet);
+
+            //    }
+            //}
+            #endregion
+
+            #region Fibonacci
+            //int a, b, c;
+            //a = 1;
+            //b = 1;
+            //c = 0;
+
+            //for (c = 0; c < 20; c++)
+            //{
+            //    c = a + b;
+            //    a = b;
+            //    b = c;
+            //    Console.WriteLine("Fibonacci dizisi: " + c);
+            //}
+
+
+            #endregion
+
+            #region Konsoldan alınan 5 sayının tek ve çift olarak ortalaması
+            //int tekToplam = 0, ciftToplam = 0;
+            //int tekAdet = 0, ciftAdet = 0;            
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Lütfen bir sayı giriniz: ");
+            //    int sayı = Convert.ToInt32(Console.ReadLine());
+
+            //    if (sayı %2 == 0)
+            //    {
+            //        ciftToplam += sayı;
+            //        ciftAdet++;
+            //    }                
+            //    else 
+            //    {
+            //        tekToplam += sayı;
+            //        tekAdet++;
+            //    }
+            //}
+            //double ciftOrtalama = ciftToplam / ciftAdet;
+            //double tekOrtalama = tekToplam / tekAdet;
+            //Console.WriteLine($"Çiftlerin ortalaması = {ciftOrtalama}");
+            //Console.WriteLine($"Teklerin ortalaması = {tekOrtalama}");
+
+            #endregion
+
+            #region Tahmin Oyunu 1
+            //Console.WriteLine("Lütfen bir sayı giriniz: ");
+            //int hedefSayı = Convert.ToInt32(Console.ReadLine());
+            //Console.Clear();
+
+            //int tahminHakki = 10;
+
+            //while (tahminHakki > 0)
+            //{
+            //    Console.WriteLine("Tahmininizi Giriniz: ");
+            //    int tahmin = Convert.ToInt32(Console.ReadLine());
+
+            //    if (tahmin > hedefSayı)
+            //    {
+            //        Console.WriteLine("Lütfen küçük sayı giriniz!");
+            //    }
+            //    if (tahmin < hedefSayı)
+            //    {
+            //        Console.WriteLine("Lütfen büyük sayı giriniz!");
+            //    }
+            //    if (tahmin == hedefSayı)
+            //    {
+            //        Console.WriteLine("Tebrikler kazandınız :) ");
+            //        return;
+            //    }
+
+            //    tahminHakki--;
+            //    Console.WriteLine("Kalan tahmin hakkınız: " + tahminHakki);
+            //}
+            //Console.WriteLine($"Üzgünüz Kaybettiniz :( Doğru sayı: {hedefSayı}");
+
+            #endregion
+
+            #region Tahmin Oyunu 2
+            Console.WriteLine("Tahmin Oyununa Hoşgeldiniz ;) ");
+            SuperMario();
+            Console.WriteLine("Lütfen bir sayı giriniz:\n");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Lütfen Zorluk Seviyesini Seçiniz");
+            Console.WriteLine("\nKolay = 1 (20 Hak) **** Orta = 2 (10 Hak) **** Zor = 3 (5 Hak)");
+            string seviyeler = Console.ReadLine();
+            Console.Clear();
+
+            int kolayHak = 20;
+            int ortaHak = 10;
+            int zorHak = 5;
+
+            while (seviyeler == "1")
+            {
+                for (int i = 1; i <= kolayHak; i++)
+                {
+                    Console.WriteLine("Lütfen Tahmin Sayınızı Giriniz!");
+                    int sayi2 = Convert.ToInt32(Console.ReadLine());
+                    if (sayi2 < sayi1)
+                    {
+                        Console.WriteLine("\nLütfen Daha Büyük Sayı Giriniz!!!");
+                        Console.Beep(100, 200);
+
+                    }
+                    else if (sayi2 > sayi1)
+                    {
+                        Console.WriteLine("\nLütfen Daha Küçük Sayı Giriniz!!!");
+                        Console.Beep(100, 200);
+                    }
+                    else if (sayi2 == sayi1)
+                    {
+                        Console.WriteLine("Tebrikler Kazandınız :) ");
+                        Console.Beep(140,600);
+                        return;
+                    }
+                    kolayHak--;
+                    Console.WriteLine("Kalan hakkınız: " + kolayHak);
+                    if (kolayHak == 0)
+                    {
+                        Console.WriteLine("Üzgünüz Kaybettiniz :{ Doğru Sayı: " + sayi1);
+                    }
+                }
+            }
+            while (seviyeler == "2")
+            {
+                for (int i = 0; i < ortaHak; i++)
+                {
+                    Console.WriteLine("Lütfen Tahmin Sayınızı Giriniz!");
+                    int sayi2 = Convert.ToInt32(Console.ReadLine());
+                    if (sayi2 < sayi1)
+                    {
+                        Console.WriteLine("\nLütfen Daha Büyük Sayı Giriniz!!!");
+                        Console.Beep(100, 200);
+
+                    }
+                    else if (sayi2 > sayi1)
+                    {
+                        Console.WriteLine("\nLütfen Daha Küçük Sayı Giriniz!!!");
+                        Console.Beep(100, 200);
+                    }
+                    else if (sayi2 == sayi1)
+                    {
+                        Console.WriteLine("Tebrikler Kazandınız :) ");
+                        Console.Beep(700, 500);
+                        return;
+                    }
+                    ortaHak--;
+                    Console.WriteLine("Kalan hakkınız: " + ortaHak);
+                    if (ortaHak == 0)
+                    {
+                        Console.WriteLine("Üzgünüz Kaybettiniz :{ Doğru Sayı: " + sayi1);
+                    }
+                }
+            }
+            while (seviyeler == "3")
+            {
+                for (int i = 1; i <= zorHak; i++)
+                {
+                    Console.WriteLine("Lütfen Tahmin Sayınızı Giriniz!");
+                    int sayi2 = Convert.ToInt32(Console.ReadLine());
+                    if (sayi2 < sayi1)
+                    {
+                        Console.WriteLine("\nLütfen Daha Büyük Sayı Giriniz!!!");
+                        Console.Beep(100, 200);
+
+                    }
+                    else if (sayi2 > sayi1)
+                    {
+                        Console.WriteLine("\nLütfen Daha Küçük Sayı Giriniz!!!");
+                        Console.Beep(100, 200);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tebrikler Kazandınız :) ");
+                        Console.Beep(700, 500);
+                        return;
+                    }
+                    zorHak--;
+                    Console.WriteLine("Kalan hakkınız: " + zorHak);
+                    if (zorHak == 0)
+                    {
+                        Console.WriteLine("Üzgünüz Kaybettiniz :{ Doğru Sayı: " + sayi1);
+                    }
+                }
+
+            }
+            #endregion
+
+            
+        }
+
+    }
+}
+
+    
+
